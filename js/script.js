b16242cc0cb90a00ce0344bc5d47af46b4ae82b3
vitali007tut <vitali007@tut.by>
2023-02-15 23:12:35 +0300

    feat: start weater section

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const slidePrev = document.querySelector('.slide-prev')
 let date
 let timeOfDay
 let hours
+const city = document.querySelector('.city')
+//city.value = 'Minsk'
 
 function showTime() {
     date = new Date()
@@ -42,6 +44,7 @@ function showGreeting(hours) {
 // set value  from localStorage
 function setLocalStorage() {
     localStorage.setItem('name', document.querySelector('input.name').value);
+    localStorage.setItem('city', document.querySelector('.city').value)
 }
 window.addEventListener('beforeunload', setLocalStorage)
 
@@ -49,6 +52,10 @@ window.addEventListener('beforeunload', setLocalStorage)
 function getLocalStorage() {
     if (localStorage.getItem('name')) {
         document.querySelector('input.name').value = localStorage.getItem('name');
+    }
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city')
+        getWeather()
     }
 }
 window.addEventListener('load', getLocalStorage)
@@ -91,6 +98,57 @@ slidePrev.addEventListener('click', getSlidePrev)
 
 // 4. Виджет погоды
 
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature')
+const weatherError = document.querySelector('.weather-error')
+const wind = document.querySelector('.wind')
+const humidity = document.querySelector('.humidity')
+const weatherDescription = document.querySelector('.weather-description')
+
+city.placeholder = `[Enter city]`
+
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=e3c0174eab562c282118d28ef2a476cd&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.cod === '404') {
+        weatherIcon.className = 'weather-icon owf'
+        temperature.textContent = null
+        wind.textContent = null
+        humidity.textContent = null
+        weatherDescription.textContent = null
+        weatherError.textContent = `Error! City not found for '${city.value}'!`
+    } else if (city.value === '') {
+        weatherIcon.className = 'weather-icon owf'
+        temperature.textContent = null
+        wind.textContent = null
+        humidity.textContent = null
+        weatherDescription.textContent = null
+        weatherError.textContent = `Error! Nothing to geocode for ''!`
+    } else {
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${data.main.temp.toFixed(0)}°C`
+        wind.textContent = `Wind speed: ${data.wind.speed.toFixed(0)} m/s`
+        humidity.textContent = `Humidity: ${data.main.humidity}%`
+        weatherDescription.textContent = data.weather[0].description;
+        weatherError.textContent = null
+    }
+
+}
+
+function setCity(event) {
+    if (event.code === 'Enter') {
+        getWeather();
+        city.blur();
+    }
+}
+
+// изначальная загрузка погоды после загрузки страницы
+document.addEventListener('DOMContentLoaded', getWeather)
+
+city.addEventListener('keypress', setCity)
 
 
 
